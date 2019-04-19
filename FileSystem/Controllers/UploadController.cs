22190f3e435ea06b51fc7308fd68d1dea17a762e
Udit Kaul <uditkaul@outlook.com>
2019-04-19 13:54:52 +0100

    Updated Upload Controller

--- FileSystem/Controllers/UploadController.cs
+++ FileSystem/Controllers/UploadController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 using Ionic.Zip;
@@ -29,42 +30,43 @@ namespace FileSystem.Controllers
             {
                 try
                 {
-                    //Method 1 Get file details from current request    
-                    //Uncomment following code if you wants to use method 1    
-                    //if (Request.Files.Count > 0)    
-                    // {    
-                    //     var Inputfile = Request.Files[0];    
-
-                    //     if (Inputfile != null && Inputfile.ContentLength > 0)    
-                    //     {    
-                    //         var filename = Path.GetFileName(Inputfile.FileName);    
-                    //       var path = Path.Combine(Server.MapPath("~/uploadedfile/"), filename);    
-                    //        Inputfile.SaveAs(path);    
-                    //    }    
-                    // }    
-
-                    //Method 2 Get file details from HttpPostedFileBase class    
-
+                  string pass = "";
                     if (file != null)
                     {
+                       
                         
                         Console.WriteLine("File type -->", file.ContentType);
                         using (ZipFile zip = new ZipFile())
                         {
+                            //Adds AES 256 encryption to the file
                             zip.Encryption = EncryptionAlgorithm.WinZipAes256;
+                            //  zip.Password = "123456";
 
-                            zip.Password = "123456";
+                            //used for creating a password for the zipped file with RNGCryptoServiceProvider library
+                            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+                            {
+                                //sets the size of the password and converts the base64 number to string to be used as password
+                                int max_length = 10;
+                                byte[] salt = new byte[max_length];
+                                rngCsp.GetNonZeroBytes(salt);
+                                String password = Convert.ToBase64String(salt);
+                                Console.Write("Password-->", password);
+                                pass = zip.Password = password;
 
 
-                            string path = Path.Combine(Server.MapPath("~/UploadedFiles"), "packed");
-                            file.SaveAs(path);
-                            zip.AddFiles(Directory.GetFiles(Server.MapPath("~/UploadedFiles/")), "packed");
-                            zip.Save(Server.MapPath("~/Encoded.zip"));
+                                string path = Path.Combine(Server.MapPath("~/UploadedFiles"), "packed");
+                                file.SaveAs(path);
+                                zip.AddFiles(Directory.GetFiles(Server.MapPath("~/UploadedFiles/")), "packed");
+                             //write logic to add timestamp
+
+                                 //saves file to /ZippedFIles folder in the root director with the origial filename in zipped format
+                                zip.Save(Server.MapPath("~/ZippedFiles/"+file.FileName+".zip"));
+                            }
 
                         }
                     }
 
-                    ViewBag.FileStatus = "File uploaded successfully.";
+                    ViewBag.FileStatus = "File uploaded successfully. -->" + pass+"-->"+file.ContentType;
                 }
                 catch (Exception)
                 {
